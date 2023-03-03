b4ed482bdb00a74df67c229a17bfea9b8e227ee7
Masroor Hussain <masroorh7@gmail.com>
2023-03-03 19:54:05 +0500

    Added Turbo and Stimulus Js to handle form submissions

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import TweetFormController from "./tweet_form_controller"
+application.register("tweet_form", TweetFormController)
